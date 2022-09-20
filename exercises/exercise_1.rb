31fc0330177195ccb9e3a985e348817dd9058e3d
DohBae <bdoherty92@msn.com>
2022-09-20 15:39:58 +0000

    Filter stores by mens/womens apparel and revenue

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,10 +5,10 @@ puts "----------"
 
 # Your code goes below here ...
 
-store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_aparel: true)
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
-store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_aparel: true)
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
-store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_aparel: false)
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.all.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where(womens_apparel: true)
+
+puts "-----MENS_STORES-----"
+@mens_stores.each do |store|
+  puts "Name: #{store.name}, Revenue: #{store.annual_revenue}"
+end
+
+puts "-----WOMENS_STORES-----"
+@womens_stores.each do |store| 
+  if (store.annual_revenue < 1000000)
+  puts "Name: #{store.name}, Revenue: #{store.annual_revenue}"
+  end
+end
